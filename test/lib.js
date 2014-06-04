cea68c7a1a2c8c003c696ec9b51ae0610a44746c
Kanstantsin Kamkou <kkamkou@gmail.com>
2014-06-04 22:30:15 +0200

    something almost working

--- test/lib.js
+++ test/lib.js
@@ -8,8 +8,17 @@
 
 // required stuff
 var path = require('path'),
-  should = require('should'),
   gelf = require(path.join('..', 'lib', 'gelf-pro'));
+
+var getLongMessage = function () {
+  var i = 0, message = '';
+  for (i = 0; i <= 10000; i++) {
+    message += "Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
+        " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an" +
+        " unknown printer took a galley of type and scrambled it to make a type specimen book.";
+  }
+  return message;
+};
 
 // tests
 module.exports = {
@@ -17,9 +26,17 @@ module.exports = {
 
   },
 
-  'Testing methods': function () {
+ 'Testing methods': function () {
     gelf.info('world', {aa: 4, bb: 5}, function (err) {
       console.log('Sent!');
+    });
+  },
+
+  'Long message split': function () {
+    var message = getLongMessage();
+
+    gelf.deflate(message, function (err, buffer) {
+      gelf.getArrayFromBuffer(buffer).should.have.length(7);
     });
   },
 
@@ -28,6 +45,7 @@ module.exports = {
     for (i = 0; i <= 1000; i++) {
       curr = gelf.getUniqueId();
       out.should.not.containEql(curr);
+      curr.should.have.length(8);
       out.push(curr);
     }
   }
